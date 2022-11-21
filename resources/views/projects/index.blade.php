8df57ad8a366badb43e9335492968114ca81965c
Javier Roman Minguez <javierroman@javierroman.com>
2022-11-21 15:36:24 +0100

    Include css styles with tailwind classes

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,11 +3,16 @@
     <div class="flex items-center mb-4">       
         <a href="/projects/create">New Project</a>
     </div>
-    <ul>
+        
+    <div class="flex">
         @forelse($projects as $project)
-            <li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
+        <div class="bg-white mr-4 rounded shadow w-1/3 p-5" style="height: 200px">
+            <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+            <div class="text-gray">{{ Illuminate\Support\Str::limit($project->description, 100) }}</div>
+        </div>
         @empty
-            <li>No projects yet.</li>
-        @endforelse
-    </ul>
+            <div>No projects yet</div>
+        @endforelse        
+    </div>    
+    
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,37 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': 'rgba(0, 0, 0, 0.4)',
+      'gray-light': '#f5f6f9',
+      'white': '#ffffff'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
