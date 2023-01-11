fdafa6ba1f64cb445f5a7d8d3d6664c65f2a0a0d
Javier Roman Minguez <javierroman@javierroman.com>
2023-01-11 16:21:15 +0100

    Refactor RecordsActivity trait

--- app/Models/RecordsActivity.php
+++ app/Models/RecordsActivity.php
@@ -10,23 +10,16 @@ trait recordsActivity
 
     public static function bootRecordsActivity()
     {
-        static::updating(function($model) {
-            $model->oldAttributes = $model->getOriginal();
-        });
-
-        if (isset(static::$recordableEvents)) {
-            $recordableEvents = static::$recordableEvents;
-        } else {
-            $recordableEvents = ['created', 'updated', 'deleted'];
-        }        
-
-        foreach($recordableEvents as $event) {
-            static::$event(function($model) use ($event) {
-                if (class_basename($model) !== 'Project') {
-                    $event = $event . '_' . strtolower(class_basename($model));
-                }  
-                $model->recordActivity($event);          
+        foreach(self::recordableEvents() as $event) {
+            static::$event(function($model) use ($event) {                   
+                $model->recordActivity($model->activityDescription($event));          
             });
+
+            if ($event == 'updated') {
+                static::updating(function($model) {
+                    $model->oldAttributes = $model->getOriginal();
+                });
+            }
         }
     }
 
@@ -43,13 +36,26 @@ trait recordsActivity
         ]);
     }
 
-    protected function activityChanges () {
+    protected function activityChanges() {
         if ($this->wasChanged()) {
             return [
                 'before' => Arr::except(array_diff($this->oldAttributes, $this->getAttributes()), 'updated_at'),
                 'after' => Arr::except($this->getChanges(), 'updated_at')
             ];
         }
+    }
+
+    protected static function recordableEvents()
+    {
+        if (isset(static::$recordableEvents)) {
+            return static::$recordableEvents;
+        } 
+        return ['created', 'updated', 'deleted'];             
+    }
+
+    protected function activityDescription($event)
+    {        
+        return $event . '_' . strtolower(class_basename($this));
     }
 
 }
